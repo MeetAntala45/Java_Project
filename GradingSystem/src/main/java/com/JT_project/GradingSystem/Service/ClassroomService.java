e1259b9b6d991cc37658972f98db893503a2b95e
MeetAntala45 <meetantala45@gmail.com>
2024-03-28 01:22:07 +0530

    Final Commit

---
+++ GradingSystem/src/main/java/com/JT_project/GradingSystem/Service/ClassroomService.java
@@ -0,0 +1,36 @@
+package com.JT_project.GradingSystem.Service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.JT_project.GradingSystem.Models.Classroom;
+import com.JT_project.GradingSystem.Models.Student;
+import com.JT_project.GradingSystem.Repos.ClassroomRepo;
+import com.JT_project.GradingSystem.utils.Result;
+import java.util.List;
+@Service
+public class ClassroomService {
+     @Autowired 
+     ClassroomRepo classroomRepo;
+     
+     public Classroom getClassById(Long class_id)
+     {
+    	 return classroomRepo.getReferenceById(class_id);
+     }
+
+     public Classroom addClassroom(Classroom classroom) {
+         return classroomRepo.save(classroom);
+     }
+     
+     public void deleteClassroom(Long classId) {
+         classroomRepo.deleteById(classId);
+     }
+     
+     public List<Classroom> getAllClassrooms() {
+         return classroomRepo.findAll();
+     }
+ 
+         
+     }
+
